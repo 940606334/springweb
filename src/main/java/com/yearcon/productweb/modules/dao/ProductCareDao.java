a28b16fad800aa9c9d7f668e8c8ed229ff3a1161
ayong <940606334@qq.com>
2018-02-06 14:28:28 +0800

    Revert "Initial commit"
    This reverts commit 806e9076f56085e01eccf745b2f602c7889ca40d.

--- src/main/java/com/yearcon/productweb/modules/dao/ProductCareDao.java
+++
@@ -1,9 +0,0 @@
-package com.yearcon.productweb.modules.dao;
-
-import com.yearcon.productweb.modules.entity.ProductCare;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
-
-public interface ProductCareDao extends CrudRepository<ProductCare,String> {
-    ProductCare findByProductNo(@Param("productNo") String productNo);
-}
