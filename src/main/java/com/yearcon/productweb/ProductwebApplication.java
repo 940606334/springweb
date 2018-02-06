806e9076f56085e01eccf745b2f602c7889ca40d
ayong <940606334@qq.com>
2018-02-06 14:28:13 +0800

    Initial commit

---
+++ src/main/java/com/yearcon/productweb/ProductwebApplication.java
@@ -0,0 +1,12 @@
+package com.yearcon.productweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductwebApplication.class, args);
+	}
+}
